1c126db0e9c50a8701e2d516c9135af4c62080dd
Pavel Logunov <paveloguniv@gmail.com>
2023-01-15 18:51:56 +0900

    feat: solve reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  let numStr = n.toString();
+  let result = '';
+  for (let i = numStr.length - 1; i >= 0; i--) {
+    let symbol = numStr[i];
+    if (symbol === '-') {
+      symbol = '';
+    }
+    result += symbol;
+  }
+  return result;
 }
+
+
